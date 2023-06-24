24f66e88d38beb7ad272b0572ea02081c8500915
Stefan Huber <stefan.huber.niedling@outlook.com>
2023-06-24 18:37:31 +0200

    Fix generation of flutter rust bindings by utilizing CPATH

--- green3neo/backend/build.rs
+++ green3neo/backend/build.rs
@@ -1,18 +1,47 @@
-use std::{process::Command, env};
+use std::process::{exit, Command};
 
 fn main() {
-    env::set_var("CPATH", "$(clang -v 2>&1 | grep \"Selected GCC installation\" | rev | cut -d' ' -f1 | rev)/include"); // FIXME Seems to have no effect
-    
-    // Execute the code generation command
-    let output = Command::new("flutter_rust_bridge_codegen")
+    let clang_output_child = Command::new("clang")
+        .args(&["-v"])
+        .output()
+        .expect("Could not collect clang output");
+
+    if !clang_output_child.status.success() {
+        eprintln!("cargo:error=Could not receive clang output");
+        exit(1);
+    }
+
+    let clang_output = String::from_utf8_lossy(&clang_output_child.stderr);
+    let mut gcc_installation_lines = clang_output
+        .lines()
+        .filter(|&line| line.starts_with("Selected GCC installation"));
+
+    let gcc_installation_line = gcc_installation_lines.next();
+
+    let additional_gcc_installation_line = gcc_installation_lines.next();
+    if additional_gcc_installation_line.is_some() {
+        eprintln!("cargo:error=The GCC installation directory is ambigous");
+        exit(1);
+    }
+
+    let gcc_include_dir = gcc_installation_line
+        .expect("Could not find GCC installation directory")
+        .rsplit_once(" ")
+        .expect("Could not remove output description")
+        .1
+        .to_owned()
+        + "/include";
+
+    // Generate flutter rust bindings
+    let generation_result = Command::new("flutter_rust_bridge_codegen")
         .args(&["-r", "./src/api.rs", "-d", "../lib/bridge_generated.dart"])
+        .env("CPATH", gcc_include_dir)
         .output()
         .expect("Failed to execute code generation command.");
 
-    if !output.status.success() {
-        // Command failed, handle the error
-        let error_message = String::from_utf8_lossy(&output.stderr);
+    if !generation_result.status.success() {
+        let error_message = String::from_utf8_lossy(&generation_result.stderr);
         eprintln!("Code generation command failed: {}", error_message);
-        std::process::exit(1);
+        exit(1);
     }
 }
