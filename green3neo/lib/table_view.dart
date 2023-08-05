0d31f017d75cafadd2793ea0ed8e98d265126461
Stefan Huber <stefan.huber.niedling@outlook.com>
2023-08-05 19:17:17 +0200

    Allow and remember cell changes

--- green3neo/lib/table_view.dart
+++ green3neo/lib/table_view.dart
@@ -22,20 +22,57 @@ class TableView extends StatelessWidget {
 class TableViewState extends ChangeNotifier {
   final List<DataColumn> columns = [];
   final List<DataRow> rows = [];
+  final Map<int, Map<int, String>> cellChanges = {};
 
   void setData(List<List<String>> data) {
     columns.clear();
     rows.clear();
 
+    /* FIXME It is assumed every row has at least the number of entries the
+     * first row has.
+     */
+
     if (data.isNotEmpty) {
-      for (String columnName in data.first) {
+      for (final String columnName in data.first) {
         columns.add(DataColumn(label: Text(columnName)));
       }
 
-      for (List<String> rowData in data.skip(1)) {
+      for (int rowIndex = 1; rowIndex < data.length; rowIndex++) {
+        final List<String> rowData = data.elementAt(rowIndex);
         final List<DataCell> cells = [];
-        for (String cellData in rowData) {
-          cells.add(DataCell(Text(cellData)));
+        for (int columnIndex = 0; columnIndex < columns.length; columnIndex++) {
+          final String cellData = rowData.elementAt(columnIndex);
+          cells.add(DataCell(
+            TextFormField(
+              initialValue: cellData,
+              decoration: InputDecoration(
+                  labelText: data.first.elementAtOrNull(columnIndex)),
+              onFieldSubmitted: (newCellValue) {
+                cellChanges.update(
+                  rowIndex,
+                  (columnChanges) {
+                    columnChanges.update(
+                      columnIndex,
+                      (oldCellValue) => newCellValue,
+                      ifAbsent: () => newCellValue,
+                    );
+                    return columnChanges;
+                  },
+                  ifAbsent: () =>
+                      Map.fromIterables([columnIndex], [newCellValue]),
+                );
+
+                print("Cell changes:");
+                for (final columnChanges in cellChanges.entries) {
+                  print("Row ${columnChanges.key.toString()}");
+                  for (final newCellValue in columnChanges.value.entries) {
+                    print(
+                        "Column ${newCellValue.key} --> ${newCellValue.value}");
+                  }
+                }
+              },
+            ),
+          ));
         }
         rows.add(DataRow(cells: cells));
       }
