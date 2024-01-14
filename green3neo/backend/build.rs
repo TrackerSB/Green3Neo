c8433df0b5199ec9c6a3529408008a4f1019ab68
Stefan Huber <stefan.huber.niedling@outlook.com>
2024-01-14 11:56:36 +0100

    Adapt cargo build script to FRB v2

--- green3neo/backend/build.rs
+++ green3neo/backend/build.rs
@@ -1,16 +1,18 @@
 use std::process::{exit, Command};
 
 fn main() {
+    println!("cargo:warning=Find GCC include dir candidates");
     let clang_output_child = Command::new("clang")
         .args(&["-v"])
         .output()
         .expect("Could not collect clang output");
 
     if !clang_output_child.status.success() {
-        eprintln!("cargo:error=Could not receive clang output");
+        println!("cargo:warning=Could not receive clang output");
         exit(1);
     }
 
+    println!("cargo:warning=Find selected GCC installation");
     let clang_output = String::from_utf8_lossy(&clang_output_child.stderr);
     let mut gcc_installation_lines = clang_output
         .lines()
@@ -20,10 +22,11 @@ fn main() {
 
     let additional_gcc_installation_line = gcc_installation_lines.next();
     if additional_gcc_installation_line.is_some() {
-        eprintln!("cargo:error=The GCC installation directory is ambigous");
+        println!("cargo:warning=The GCC installation directory is ambigous");
         exit(1);
     }
 
+    println!("cargo:warning=Determine GCC installation include dir");
     let gcc_include_dir = gcc_installation_line
         .expect("Could not find GCC installation directory")
         .rsplit_once(" ")
@@ -32,17 +35,17 @@ fn main() {
         .to_owned()
         + "/include";
 
-    // Generate flutter rust bindings
-    let generation_result = Command::new("flutter_rust_bridge_codegen")
-        .args(&["-r", "./src/api.rs", "-d", "../lib/bridge_generated.dart"])
+    println!("cargo:warning=Generate flutter rust bindings");
+    let frb_generation_result = Command::new("flutter_rust_bridge_codegen")
+        .args(&["generate", "-r", "./src/api.rs", "-d", "../lib/"])
         .env("CPATH", gcc_include_dir)
-        .env("RUST_BACKTRACE", "1")
+        .env("RUST_BACKTRACE", "full")
         .output()
-        .expect("Failed to execute code generation command.");
+        .expect("Failed to execute FRB code generation command");
 
-    if !generation_result.status.success() {
-        let error_message = String::from_utf8_lossy(&generation_result.stderr);
-        eprintln!("Code generation command failed: {}", error_message);
+    if !frb_generation_result.status.success() {
+        let error_message = String::from_utf8_lossy(&frb_generation_result.stderr);
+        println!("cargo:warning=FRB code generation failed: {}", error_message);
         exit(1);
     }
 }
