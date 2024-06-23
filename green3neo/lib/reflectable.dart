5dcae3846968bc63c834a1d3571473ca8b6f6a16
Stefan Huber <stefan.huber.niedling@outlook.com>
2024-06-23 11:40:41 +0200

    Fix required capabilities

--- green3neo/lib/reflectable.dart
+++ green3neo/lib/reflectable.dart
@@ -1,7 +1,11 @@
 import 'package:reflectable/reflectable.dart';
 
 class Reflector extends Reflectable {
-  const Reflector() : super(declarationsCapability);
+  const Reflector()
+      : super(
+            declarationsCapability, // For allowing access to declarations
+            instanceInvokeCapability // For allowing reading declarations (fields and methods)
+            );
 }
 
 const reflectableMarker = Reflector();
