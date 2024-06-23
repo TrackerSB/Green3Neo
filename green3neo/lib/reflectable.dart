9b6eba86ec02b8af2825938d2d1b6c4005f7c4b8
TrackerSB <junktogo@web.de>
2024-06-23 12:46:42 +0200

    Revert "Visualize fields of database scheme on GUI (#12)"
    This reverts commit 06d00ce2e0d8dc7df5e888ed5c15c8978cda6b1b.

--- green3neo/lib/reflectable.dart
+++ green3neo/lib/reflectable.dart
@@ -1,12 +1,7 @@
 import 'package:reflectable/reflectable.dart';
 
 class Reflector extends Reflectable {
-  const Reflector()
-      : super(
-            declarationsCapability, // For allowing access to declarations
-            instanceInvokeCapability, // For allowing reading declarations (fields and methods)
-            reflectedTypeCapability, // For allowing access to types of fields
-            );
+  const Reflector() : super(declarationsCapability);
 }
 
 const reflectableMarker = Reflector();
--- green3neo/tasks/populate_db_tables.zsh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-source ./subtasks/activateVenv.zsh
-
-# python ./delete_db_tables.py
-# python ./create_db_tables.py
-python ./populate_db_tables.py
-
-source ./subtasks/deactivateVenv.zsh
