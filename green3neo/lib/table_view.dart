fd9edd9f04d6602fb47b9d7269b3de5937fd90a0
Stefan Huber <stefan.huber.niedling@outlook.com>
2024-09-30 09:48:32 +0200

    Remove unused import

--- green3neo/lib/table_view.dart
+++ green3neo/lib/table_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 import 'package:reflectable/mirrors.dart';
 import 'reflectable.dart';
