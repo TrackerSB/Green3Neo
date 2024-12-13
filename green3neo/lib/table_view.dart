b5ee84519dc66ed20469f0cc4fc03e1b3a090a04
Stefan Huber <stefan.huber.niedling@outlook.com>
2024-12-13 18:20:05 +0100

    Move class independent function out of class

--- green3neo/lib/table_view.dart
+++ green3neo/lib/table_view.dart
@@ -83,190 +83,190 @@ Widget generateUnsupportedPopup<DataObject extends Object>(
   return Text(initialValue.toString());
 }
 
-class TableView<DataObject extends Object> extends StatelessWidget {
-  const TableView({super.key});
+DataCellGenerator<DataObject> _wrapIntoCellGenerator<DataObject extends Object>(
+    BuildContext context,
+    CellPopupGenerator<SupportedType?> popupGenerator,
+    DataColumnInfo<DataObject, SupportedType> info) {
+  return (DataObject object) {
+    SupportedType? initialCellValue = info.getter(object);
+    SupportedType? currentCellValue = initialCellValue;
+    // TODO Reflect data cell changes in data objects
 
-  DataCellGenerator<DataObject> _wrapIntoCellGenerator(
-      BuildContext context,
-      CellPopupGenerator<SupportedType?> popupGenerator,
-      DataColumnInfo<DataObject, SupportedType> info) {
-    return (DataObject object) {
-      SupportedType? initialCellValue = info.getter(object);
-      SupportedType? currentCellValue = initialCellValue;
-      // TODO Reflect data cell changes in data objects
-
-      StateSetter setCellState = (setter) {
-        // WARN Setter is executed without actually updating cell state
-        setter();
-      };
-
-      onValueChange(newCellValue) {
-        setCellState(() => currentCellValue = newCellValue);
-      }
-
-      onValueSubmit() {
-        initialCellValue = currentCellValue;
-        Navigator.pop(context);
-      }
-
-      return DataCell(
-        StatefulBuilder(
-          builder: (BuildContext context, StateSetter setState) {
-            setCellState = setState;
-
-            dynamic cellValue = currentCellValue?.value;
-            if (cellValue == null) {
-              return const Text("null");
-            }
-
-            return Text(cellValue.toString());
-          },
-        ),
-        onTap: () {
-          showGeneralDialog(
-            context: context,
-            pageBuilder: (context, animation, secondaryAnimation) {
-              return Dialog(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(10),
-                      child: popupGenerator(
-                          currentCellValue, onValueChange, onValueSubmit),
-                    ),
-                    Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        ElevatedButton(
-                          // FIXME Save button does not actually save
-                          onPressed: onValueSubmit,
-                          child: const Text("Save"),
-                        ),
-                        ElevatedButton(
-                          onPressed: () {
-                            onValueChange(initialCellValue);
-                            onValueSubmit();
-                          },
-                          child: const Text("Cancel"),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              );
-            },
-          );
-        },
-      );
+    StateSetter setCellState = (setter) {
+      // WARN Setter is executed without actually updating cell state
+      setter();
     };
-  }
 
-  CellPopupGenerator<SupportedType?> _wrapIntoNullPopupGenerator(
-      CellPopupGenerator<SupportedType> popupGenerator,
-      DataColumnInfo<DataObject, SupportedType> info) {
-    return (SupportedType? currentValue, CellValueHandler onValueChange,
-        CellSubmitHandler onValueSubmit) {
-      StateSetter setCellState = (setter) {
-        // WARN Setter is executed without actually updating cell state
-        setter();
-      };
+    onValueChange(newCellValue) {
+      setCellState(() => currentCellValue = newCellValue);
+    }
 
-      onChanged(isChecked) {
-        setCellState(() {
-          if (isChecked) {
-            currentValue = info.supportedType.when(
-              int: (value) => const SupportedType.int(0),
-              string: (value) => const SupportedType.string(""),
-              bool: (value) => const SupportedType.bool(false),
-              unsupported: SupportedType.unsupported,
-            );
-          } else {
-            currentValue = null;
-          }
-          onValueChange(currentValue);
-        });
-      }
+    onValueSubmit() {
+      initialCellValue = currentCellValue;
+      Navigator.pop(context);
+    }
 
-      return StatefulBuilder(
+    return DataCell(
+      StatefulBuilder(
         builder: (BuildContext context, StateSetter setState) {
           setCellState = setState;
 
-          return Row(
-            children: [
-              Checkbox(
-                value: currentValue != null,
-                onChanged: onChanged,
-              ),
-              Expanded(
-                  child: (currentValue == null)
-                      ? const Text("Value is currently null")
-                      : popupGenerator(
-                          currentValue!, onValueChange, onValueSubmit)),
-            ],
-          );
+          dynamic cellValue = currentCellValue?.value;
+          if (cellValue == null) {
+            return const Text("null");
+          }
+
+          return Text(cellValue.toString());
         },
-      );
-    };
-  }
-
-  DataCellGenerator<DataObject> _createCellGenerator(
-      BuildContext context, DataColumnInfo<DataObject, SupportedType> info) {
-    final isNullableType = info.typeMirror.isNullable;
-
-    popupGenerator(SupportedType? initialValue, CellValueHandler onValueChange,
-        CellSubmitHandler onValueSubmit) {
-      // ignore: null_check_on_nullable_type_parameter
-      return info.supportedType
-          .generateCellPopup(initialValue!, onValueChange, onValueSubmit);
-    }
-
-    CellPopupGenerator<SupportedType?> nullablePopupGenerator;
-    if (isNullableType) {
-      nullablePopupGenerator =
-          _wrapIntoNullPopupGenerator(popupGenerator, info);
-    } else {
-      nullablePopupGenerator = popupGenerator;
-    }
-
-    return _wrapIntoCellGenerator(context, nullablePopupGenerator, info);
-  }
-
-  List<DataCellGenerator<DataObject>> _createCellGenerators(
-      BuildContext context,
-      Map<String, DataColumnInfo<DataObject, SupportedType>> columnInfo) {
-    return columnInfo
-        .map(
-          (name, info) {
-            DataCellGenerator<DataObject> generator = () {
-              return info.supportedType.when(
-                int: (value) => _createCellGenerator(context, info),
-                bool: (value) => _createCellGenerator(context, info),
-                string: (value) => _createCellGenerator(context, info),
-                unsupported: (value) => _createCellGenerator(context, info),
-              );
-            }();
-            return MapEntry(name, generator);
+      ),
+      onTap: () {
+        showGeneralDialog(
+          context: context,
+          pageBuilder: (context, animation, secondaryAnimation) {
+            return Dialog(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: popupGenerator(
+                        currentCellValue, onValueChange, onValueSubmit),
+                  ),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      ElevatedButton(
+                        // FIXME Save button does not actually save
+                        onPressed: onValueSubmit,
+                        child: const Text("Save"),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          onValueChange(initialCellValue);
+                          onValueSubmit();
+                        },
+                        child: const Text("Cancel"),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            );
           },
-        )
-        .values
-        .toList();
+        );
+      },
+    );
+  };
+}
+
+CellPopupGenerator<SupportedType?>
+    _wrapIntoNullPopupGenerator<DataObject extends Object>(
+        CellPopupGenerator<SupportedType> popupGenerator,
+        DataColumnInfo<DataObject, SupportedType> info) {
+  return (SupportedType? currentValue, CellValueHandler onValueChange,
+      CellSubmitHandler onValueSubmit) {
+    StateSetter setCellState = (setter) {
+      // WARN Setter is executed without actually updating cell state
+      setter();
+    };
+
+    onChanged(isChecked) {
+      setCellState(() {
+        if (isChecked) {
+          currentValue = info.supportedType.when(
+            int: (value) => const SupportedType.int(0),
+            string: (value) => const SupportedType.string(""),
+            bool: (value) => const SupportedType.bool(false),
+            unsupported: SupportedType.unsupported,
+          );
+        } else {
+          currentValue = null;
+        }
+        onValueChange(currentValue);
+      });
+    }
+
+    return StatefulBuilder(
+      builder: (BuildContext context, StateSetter setState) {
+        setCellState = setState;
+
+        return Row(
+          children: [
+            Checkbox(
+              value: currentValue != null,
+              onChanged: onChanged,
+            ),
+            Expanded(
+                child: (currentValue == null)
+                    ? const Text("Value is currently null")
+                    : popupGenerator(
+                        currentValue!, onValueChange, onValueSubmit)),
+          ],
+        );
+      },
+    );
+  };
+}
+
+DataCellGenerator<DataObject> _createCellGenerator<DataObject extends Object>(
+    BuildContext context, DataColumnInfo<DataObject, SupportedType> info) {
+  final isNullableType = info.typeMirror.isNullable;
+
+  popupGenerator(SupportedType? initialValue, CellValueHandler onValueChange,
+      CellSubmitHandler onValueSubmit) {
+    // ignore: null_check_on_nullable_type_parameter
+    return info.supportedType
+        .generateCellPopup(initialValue!, onValueChange, onValueSubmit);
   }
 
-  List<DataColumn> _createColumns(Map<String, TypeMirror> columnInfo) {
-    return columnInfo
-        .map<String, DataColumn>((name, type) {
-          return MapEntry(
-            name,
-            DataColumn(
-              label: Text(name),
-            ),
-          );
-        })
-        .values
-        .toList();
+  CellPopupGenerator<SupportedType?> nullablePopupGenerator;
+  if (isNullableType) {
+    nullablePopupGenerator = _wrapIntoNullPopupGenerator(popupGenerator, info);
+  } else {
+    nullablePopupGenerator = popupGenerator;
   }
+
+  return _wrapIntoCellGenerator(context, nullablePopupGenerator, info);
+}
+
+List<DataCellGenerator<DataObject>>
+    _createCellGenerators<DataObject extends Object>(BuildContext context,
+        Map<String, DataColumnInfo<DataObject, SupportedType>> columnInfo) {
+  return columnInfo
+      .map(
+        (name, info) {
+          DataCellGenerator<DataObject> generator = () {
+            return info.supportedType.when(
+              int: (value) => _createCellGenerator(context, info),
+              bool: (value) => _createCellGenerator(context, info),
+              string: (value) => _createCellGenerator(context, info),
+              unsupported: (value) => _createCellGenerator(context, info),
+            );
+          }();
+          return MapEntry(name, generator);
+        },
+      )
+      .values
+      .toList();
+}
+
+List<DataColumn> _createColumns(Map<String, TypeMirror> columnInfo) {
+  return columnInfo
+      .map<String, DataColumn>((name, type) {
+        return MapEntry(
+          name,
+          DataColumn(
+            label: Text(name),
+          ),
+        );
+      })
+      .values
+      .toList();
+}
+
+class TableView<DataObject extends Object> extends StatelessWidget {
+  const TableView({super.key});
 
   @override
   Widget build(BuildContext context) {
