e356bbd67f2c5d459cc0e5f58608cf9cbdc72d9d
Stefan Huber <stefan.huber.niedling@outlook.com>
2024-12-13 11:18:22 +0100

    Move database tasks to justfile

--- green3neo/tasks/create_db_tables.zsh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-
-source ./subtasks/activateVenv.zsh
-
-python ./create_db_tables.py
-
-source ./subtasks/deactivateVenv.zsh
--- green3neo/tasks/delete_db_tables.zsh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-
-source ./subtasks/activateVenv.zsh
-
-python ./delete_db_tables.py
-
-source ./subtasks/deactivateVenv.zsh
