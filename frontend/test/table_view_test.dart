9062845974ff39df3e626a8942c0b60a8d36f626
Stefan Huber <stefan.huber.niedling@outlook.com>
2025-04-06 13:45:21 +0200

    Introduce dummy test for frontend

---
+++ frontend/test/table_view_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets("Dummy", (tester) async {
+    // This is a dummy test to ensure that the test suite runs without errors.
+    // You can replace this with actual tests for your table view.
+    expect(true, true);
+  });
+}
