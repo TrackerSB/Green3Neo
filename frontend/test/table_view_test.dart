5af481d94ffb86984d569540b36a7edfbac4c63c
Stefan Huber <stefan.huber.niedling@outlook.com>
2025-04-06 15:56:23 +0200

    Prefer actual "close" button over "some" button that closes the dialog

--- frontend/test/table_view_test.dart
+++ frontend/test/table_view_test.dart
@@ -1,9 +1,28 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:green3neo/table_view.dart';
 
 void main() {
-  testWidgets("Dummy", (tester) async {
-    // This is a dummy test to ensure that the test suite runs without errors.
-    // You can replace this with actual tests for your table view.
-    expect(true, true);
+  testWidgets("Verify common properties and behavior of table view popups",
+      (tester) async {
+    final value = createDefaultValue<UnsupportedVariant>();
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: TableViewUnsupportedCellPopup(
+          initialValue: UnsupportedVariant(value),
+          isNullable: false,
+          onCellValueSubmitted: (submittedValue) {},
+        ),
+      ),
+    );
+
+    // FIXME Verify changing text
+    // FIXME Verify nullable property
+    // FIXME Verify onCellValueSubmitted property
+
+    expect(find.text(value.toString()), findsOneWidget);
+    expect(find.closeButton(), findsOneWidget);
+    expect(find.text("Save"), findsOneWidget);
   });
 }
