cff35546bc4f2961c88620326d647534d28ae292
Stefan Huber <stefan.huber.niedling@outlook.com>
2025-05-11 11:19:31 +0200

    Verify behavior of nullable checkbox

--- frontend/test/table_view_test.dart
+++ frontend/test/table_view_test.dart
@@ -28,4 +28,60 @@ void main() {
     expect(find.closeButton(), findsOneWidget);
     expect(find.text("Save"), findsOneWidget);
   });
+
+  testWidgets("Verify behavior on (un-)setting value to null on nullable field",
+      (tester) async {
+    // FIXME Verify all SupportedTypes
+    final initialValue = createDefaultValue<UnsupportedVariant>();
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: TableViewUnsupportedCellPopup(
+            initialValue: initialValue,
+            isNullable: true,
+            onCellValueSubmitted: (submittedValue) {},
+          ),
+        ),
+      ),
+    );
+
+    final popupFinder = find.byType(TableViewUnsupportedCellPopup);
+    expect(popupFinder, findsOneWidget);
+
+    verifyCellValue(UnsupportedVariant? expectedValue) {
+      final TableViewUnsupportedCellPopup popup = tester.widget(popupFinder);
+      expect(popup.initialValue, expectedValue);
+      // FIXME Verify editability
+    }
+
+    final nullableCheckBoxFinder = find.byType(Checkbox);
+    expect(nullableCheckBoxFinder, findsOneWidget);
+
+    verifyCheckboxState(bool isChecked) {
+      final Checkbox checkBox = tester.widget(nullableCheckBoxFinder);
+      expect(checkBox.value, isChecked);
+      expect(checkBox.tristate, isFalse);
+      // FIXME Verify editability
+    }
+
+    verifyCellValue(initialValue);
+    verifyCheckboxState(true);
+
+    // Disable checkbox
+    await tester.tap(nullableCheckBoxFinder);
+    await tester.pumpAndSettle();
+    verifyCellValue(initialValue);
+    verifyCheckboxState(false);
+
+    // Re-enable checkbox
+    await tester.tap(nullableCheckBoxFinder);
+    await tester.pumpAndSettle();
+    verifyCellValue(initialValue);
+    verifyCheckboxState(true);
+  });
+
+  testWidgets("Set null to non-nullable widget", (tester) async {
+    // FIXME Implement
+  });
 }
