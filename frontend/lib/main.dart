aec4f039ebe330e8e9071ca5fb94035898dc5619
TrackerSB <stefan.huber.niedling@outlook.com>
2025-10-27 12:42:53 +0100

    GitHub: Introduce workflow starting the application (#54)
    Close #50

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -3,12 +3,32 @@ import 'package:green3neo/member_management_page.dart';
 import 'package:green3neo/backend_api/frb_generated.dart' as backend_api;
 import 'package:green3neo/database_api/frb_generated.dart' as database_api;
 import 'main.reflectable.dart';
+import 'dart:io' show Platform;
+import 'package:window_manager/window_manager.dart';
+
+const String windowTitle = "Green3Neo"; // FIXME Localize text
 
 void main() async {
   initializeReflectable();
 
   await backend_api.RustLib.init();
   await database_api.RustLib.init();
+
+  bool isDesktop = Platform.isWindows || Platform.isLinux || Platform.isMacOS;
+  if (isDesktop) {
+    WidgetsFlutterBinding.ensureInitialized();
+    await windowManager.ensureInitialized();
+
+    WindowOptions windowOptions = const WindowOptions(
+      center: true,
+      title: windowTitle,
+    );
+
+    windowManager.waitUntilReadyToShow(windowOptions, () async {
+      await windowManager.show();
+      await windowManager.focus();
+    });
+  }
 
   runApp(const MainApp());
 }
@@ -19,6 +39,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      title: windowTitle,
       home: Scaffold(
         body: MemberManagementPage(),
       ),
