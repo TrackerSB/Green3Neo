afc62e805118aca38c95415d2bbf5b190de27879
Stefan Huber <stefan.huber.niedling@outlook.com>
2026-01-17 11:21:14 +0100

    Make message ID configurable

---
+++ frontend/lib/components/form_fields/message_id_field.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:green3neo/components/form_fields/form_text_field.dart';
+import 'package:green3neo/localizer.dart';
+
+class MessageIdField extends FormTextField<String> {
+  MessageIdField({super.key})
+      : super(
+          convert: (final String value) => value,
+          labelText: Localizer.instance.text((l) => l.messageId),
+          invalidText: Localizer.instance.text((l) => l.invalidMessageId),
+          keyboardType: TextInputType.text,
+          // FIXME Introduce regex in backend and call match function in frontend
+          validate: (final String value) => value.isNotEmpty,
+        );
+}
--- frontend/lib/features/management_mode/sepa_management/sepa_generation_wizard.dart
+++ frontend/lib/features/management_mode/sepa_management/sepa_generation_wizard.dart
@@ -7,6 +7,7 @@ import 'package:green3neo/components/form_fields/creditor_iban_field.dart';
 import 'package:green3neo/components/form_fields/creditor_id_field.dart';
 import 'package:green3neo/components/form_fields/creditor_name_field.dart';
 import 'package:green3neo/components/form_fields/currency_field.dart';
+import 'package:green3neo/components/form_fields/message_id_field.dart';
 import 'package:green3neo/components/form_fields/purpose_field.dart';
 import 'package:green3neo/database_api/api/models.dart';
 import 'package:green3neo/features/feature.dart';
@@ -21,8 +22,12 @@ import 'package:path_provider/path_provider.dart';
 // FIXME Determine DART file name automatically
 final _logger = Logger("sepa_generation_wizard");
 
-Future<String> _generateSepaContent(final Creditor creditor,
-    final List<Member> member, final double value, final String purpose) {
+Future<String> _generateSepaContent(
+    final String messageId,
+    final Creditor creditor,
+    final List<Member> member,
+    final double value,
+    final String purpose) {
   final transactions = member.map(
     (final Member m) {
       final mandate = Mandate(
@@ -44,8 +49,7 @@ Future<String> _generateSepaContent(final Creditor creditor,
     },
   ).toList();
   return generateSepaDocument(
-      // FIXME Make message ID configurable
-      messageId: "2026-01-09_FancyMessageID",
+      messageId: messageId,
       collectionDateUtc: DateTime.now().toUtc(),
       creditor: creditor,
       transactions: transactions);
@@ -89,6 +93,7 @@ class SepaGenerationWizard extends StatelessWidget {
   SepaGenerationWizard._create({super.key, required this.member});
 
   void _onOkButtonPressed(
+      final MessageIdField messageIdField,
       final CreditorNameField creditorNameField,
       final CreditorIbanField creditorIbanField,
       final CreditorIdField creditorIdField,
@@ -103,12 +108,14 @@ class SepaGenerationWizard extends StatelessWidget {
 
     formState.save();
 
+    final String? messageId = messageIdField.value.value;
     final double? amount = currencyField.value.value;
     final String? creditorName = creditorNameField.value.value;
     final String? creditorIban = creditorIbanField.value.value;
     final String? creditorId = creditorIdField.value.value;
     final String? purpose = purposeField.value.value;
-    if ((amount == null) ||
+    if ((messageId == null) ||
+        (amount == null) ||
         (creditorName == null) ||
         (creditorIban == null) ||
         (creditorId == null) ||
@@ -122,7 +129,7 @@ class SepaGenerationWizard extends StatelessWidget {
         Creditor(name: creditorName, id: creditorId, iban: creditorIban);
 
     final Future<String> sepaContent =
-        _generateSepaContent(creditor, member, amount, purpose);
+        _generateSepaContent(messageId, creditor, member, amount, purpose);
     final Future<String?> outputPath = _askUserForOutputPath();
 
     await _writeContentToPath(sepaContent, outputPath);
@@ -134,6 +141,7 @@ class SepaGenerationWizard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final messageIdField = MessageIdField();
     final currencyField = CurrencyField();
     final purposeField = PurposeField();
     final creditorNameField = CreditorNameField();
@@ -150,6 +158,7 @@ class SepaGenerationWizard extends StatelessWidget {
               children: [
                 Text(Localizer.instance.text(
                     (l) => l.numMembersSelected(numSelected: member.length))),
+                messageIdField,
                 creditorNameField,
                 creditorIbanField,
                 creditorIdField,
@@ -162,6 +171,7 @@ class SepaGenerationWizard extends StatelessWidget {
             children: [
               ElevatedButton(
                 onPressed: () => _onOkButtonPressed(
+                    messageIdField,
                     creditorNameField,
                     creditorIbanField,
                     creditorIdField,
