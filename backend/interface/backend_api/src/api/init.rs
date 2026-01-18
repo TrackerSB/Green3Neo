2643fb7d9f3f9941fb1d859371b7b611b0a43468
Stefan Huber <stefan.huber.niedling@outlook.com>
2026-01-18 19:32:23 +0100

    [Temp]: Print file permissions of log directory

--- backend/interface/backend_api/src/api/init.rs
+++ backend/interface/backend_api/src/api/init.rs
@@ -1,11 +1,18 @@
 use std::sync::LazyLock;
 
+use backend_logging::logging::create_logger;
 use flexi_logger::LoggerHandle;
 use flutter_rust_bridge::frb;
-use backend_logging::logging::create_logger;
 
 #[frb(init)]
 pub fn init_app() {
     static LOGGER: LazyLock<LoggerHandle> = LazyLock::new(|| create_logger(None));
     LOGGER.flush(); // Trigger creation of logger
+
+    let metadata = std::fs::metadata("/home/runner/.local/share/green3neo/logs");
+    if metadata.is_ok() {
+        println!("Metadata of log dir: {:?}", metadata);
+    } else {
+        println!("Could not retrieve metadata of log dir");
+    }
 }
