ee68483a0255f3ad27b32ddef56c4300bd41a9c5
Stefan Huber <stefan.huber.niedling@outlook.com>
2026-02-13 21:42:25 +0100

    Fix FRB capability of Creditor

--- backend/sepa_types/src/creditor.rs
+++ backend/sepa_types/src/creditor.rs
@@ -2,7 +2,8 @@ use serde::{Deserialize, Serialize};
 
 use crate::{creditor_id::CreditorID, iban::IBAN, name::Name};
 
-#[derive(Debug, Serialize, Deserialize)]
+// FIXME Can deriving from Clone be avoided?
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Creditor {
     pub name: Name,
     pub id: CreditorID,
--- backend/sepa_types/src/creditor_id.rs
+++ backend/sepa_types/src/creditor_id.rs
@@ -1,7 +1,8 @@
 use serde::{Deserialize, Serialize};
 
 // FIXME Add checks for valid creditor IDs
-#[derive(Debug, Serialize, Deserialize)]
+// FIXME Can deriving from Clone be avoided?
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct CreditorID {
     pub value: String,
 }
