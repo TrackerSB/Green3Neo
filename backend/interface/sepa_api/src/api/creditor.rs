0e17273c5850f2e3aa733594eac3fbd100421d47
Stefan Huber <stefan.huber.niedling@outlook.com>
2026-02-13 10:22:49 +0100

    Deserialize creditor using toml directly instead of Config object workaround

--- backend/interface/sepa_api/src/api/creditor.rs
+++ backend/interface/sepa_api/src/api/creditor.rs
@@ -1,5 +1,7 @@
+use std::fs;
+
 use backend_paths::paths::get_user_config_dir;
-use config::{Config, Environment, File, FileFormat, FileStoredFormat};
+use config::{FileFormat, FileStoredFormat};
 use log::warn;
 use serde::{Deserialize, Serialize};
 
@@ -20,29 +22,32 @@ static CREDITOR_CONFIG_FILE_STEM: &str = "creditor";
 static CREDITOR_CONFIG_FILE_FORMAT: FileFormat = FileFormat::Toml;
 
 pub fn get_configured_creditor() -> Option<Creditor> {
-    let config_save_path = get_user_config_dir().join(CREDITOR_CONFIG_FILE_STEM);
-    let config = Config::builder()
-        .add_source(File::new(
-            config_save_path.to_str().unwrap(),
-            CREDITOR_CONFIG_FILE_FORMAT,
-        ))
-        .add_source(Environment::with_prefix(CREDITOR_CONFIG_FILE_STEM))
-        .build()
-        .expect("Could not read configuration");
+    let config_save_path = get_user_config_dir()
+        .join(CREDITOR_CONFIG_FILE_STEM)
+        .with_extension(CREDITOR_CONFIG_FILE_FORMAT.file_extensions()[0]);
+    let serialized_creditor = fs::read_to_string(&config_save_path);
 
-    let creditor_name = config.get_string("name");
-    let creditor_id = config.get_string("id");
-    let creditor_iban = config.get_string("iban");
-
-    if creditor_name.is_ok() && creditor_id.is_ok() && creditor_iban.is_ok() {
-        return Some(Creditor {
-            name: creditor_name.unwrap(),
-            id: creditor_id.unwrap(),
-            iban: creditor_iban.unwrap(),
-        });
+    if serialized_creditor.is_err() {
+        warn!(
+            "Could not read '{}' due to '{}'",
+            config_save_path.display(),
+            serialized_creditor.err().unwrap()
+        );
+        return None;
     }
 
-    None
+    let deserialized_creditor = toml::from_str(serialized_creditor.as_ref().unwrap());
+
+    if deserialized_creditor.is_err() {
+        warn!(
+            "Could not deserialize creditor from string '{}' due to '{}'",
+            serialized_creditor.unwrap(),
+            deserialized_creditor.err().unwrap()
+        );
+        return None;
+    }
+
+    deserialized_creditor.unwrap()
 }
 
 pub fn set_configured_creditor(creditor: Creditor) {
