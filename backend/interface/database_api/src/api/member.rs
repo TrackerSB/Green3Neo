0ed81828b3a8579864dc768073daa48e5a68dbbe
Stefan Huber <stefan.huber.niedling@outlook.com>
2026-04-02 11:31:02 +0200

    Remove unused import

--- backend/interface/database_api/src/api/member.rs
+++ backend/interface/database_api/src/api/member.rs
@@ -1,7 +1,6 @@
 use crate::database::bind_column_value;
 use crate::{api::models::Member, connection::get_connection};
 use database_types::connection_description::ConnectionDescription;
-use diesel::mysql::Mysql;
 use diesel::{RunQueryDsl, SelectableHelper, query_dsl::methods::SelectDsl, sql_types::Integer};
 use log::{error, info, warn};
 
