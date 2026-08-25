f21ac750bbd485d3988940246172141d0ba85f00
Stefan Huber <stefan.huber.niedling@outlook.com>
2026-08-25 21:16:25 +0200

    Do not register features multiple times

--- frontend/lib/features/frontend_feature.dart
+++ frontend/lib/features/frontend_feature.dart
@@ -3,12 +3,20 @@ import 'package:green3neo/features/loaded_profile.dart';
 import 'package:green3neo/interface/backend_api/api/feature.dart';
 
 abstract class FrontendFeature {
+  static bool _featureRegistered = false;
+
   // Do not override this method
   Future<Null> register() {
     final getIt = GetIt.instance;
+
+    if (_featureRegistered) {
+      return Future.value();
+    }
+
     return getIt.getAsync<LoadedProfile>().then((LoadedProfile profile) {
       if (profile.features.contains(requiredFeature())) {
         registerUnconditionally();
+        _featureRegistered = true;
       }
     });
   }
