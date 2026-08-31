f1294f559b3b78af08612f505682d41e2d4182da
Stefan Huber <stefan.huber.niedling@outlook.com>
2026-08-31 20:09:26 +0200

    Introduce stub for feature graph

--- frontend/lib/features/feature_view.dart
+++ frontend/lib/features/feature_view.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
+import 'package:graphview/GraphView.dart';
 import 'package:green3neo/features/widget_feature.dart';
 import 'package:green3neo/interface/backend_api/api/feature.dart';
 import 'package:watch_it/watch_it.dart';
@@ -9,7 +10,33 @@ class FeatureSettingsPage extends WatchingWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text("HERE");
+    final Node nodeA = Node.Id("nodeA");
+    final Node nodeB = Node.Id("nodeB");
+
+    var graph = Graph();
+    graph.addEdge(nodeA, nodeB);
+
+    final algorithmConfig = SugiyamaConfiguration()
+      ..nodeSeparation = 15
+      ..levelSeparation = 15
+      ..orientation = SugiyamaConfiguration.ORIENTATION_TOP_BOTTOM;
+
+    return Scaffold(
+      backgroundColor: Colors.amber,
+      body: GraphView.builder(
+        graph: graph,
+        algorithm: SugiyamaAlgorithm(algorithmConfig),
+        builder: (node) {
+          final nodeId = node.key!.value as String;
+          return Container(
+            width: 40,
+            height: 40,
+            decoration: BoxDecoration(color: Colors.blue),
+            child: Center(child: Text(nodeId)),
+          );
+        },
+      ),
+    );
   }
 }
 
